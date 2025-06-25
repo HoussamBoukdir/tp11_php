03e0af06f161ccf41c065938519fedad409ae713
HoussamBoukdir <boukdir.houssam166@gmail.com>
2025-06-26 00:29:43 +0100

    Add files via upload

---
+++ function1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>function</title>
+</head>
+<body>
+    <h3>les fonctions en PHP</h3>
+    <?php 
+        $tab=["Dev", "Res", "Test"];
+        echo "<pre>";
+        print_r($tab);
+        echo"</pre>"
+    ?>
+</body>
+</html>
